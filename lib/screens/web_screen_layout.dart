a193f26ff74818e7020fe8114c99a625ce5820c2
alexacolbe <ibegbulam.ua@gmail.com>
2023-01-31 23:10:40 +0100

    web screen layout

--- lib/screens/web_screen_layout.dart
+++ lib/screens/web_screen_layout.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:whatsappclone/widgets/contact_list.dart';
+import 'package:whatsappclone/widgets/web_profile_bar.dart';
+import 'package:whatsappclone/widgets/web_search_bar.dart';
 
 class WebScreenLayout extends StatelessWidget {
   const WebScreenLayout({super.key});
@@ -10,14 +12,29 @@ class WebScreenLayout extends StatelessWidget {
       body: Row(
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-           Column(
-            children: [
-              // web pprofile bar
-              // web search bar
-              ContactList(),
-            ],
+           Expanded(
+             child: SingleChildScrollView(
+               child: Column(
+                children: [
+                  // web pprofile bar
+
+                  WebProfileBar(),
+                  // web search bar
+                  WebSearchBar(),
+                  ContactList(),
+                ],
+               ),
+             ),
            ),
            // web screen
+           Container(
+            width: MediaQuery.of(context).size.width*0.75,
+            decoration: BoxDecoration(
+              image: DecorationImage(image: AssetImage('images/whatsappbg.png',
+              ),
+              fit: BoxFit.cover),
+             )
+           ),
         ],
       )
     );
--- lib/widgets/contact_list.dart
+++ lib/widgets/contact_list.dart
@@ -9,51 +9,48 @@ class ContactList extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.only(top: 10),
-      child: Column(
-        children: [
-          Expanded(
-            child: ListView.builder(
-              shrinkWrap: true,
-              itemCount: info.length,
-              itemBuilder: (context, index) {
-                return InkWell(
-                  onTap: () {},
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: ListTile(
-                      title: Text(
-                        info[index]['name'].toString(),
-                        style: const TextStyle(fontSize: 18),
-                      ),
-                      subtitle: Padding(
-                        padding: const EdgeInsets.only(top: 6),
-                        child: Text(
-                          info[index]['message'].toString(),
-                          style: const TextStyle(fontSize: 15),
-                        ),
-                      ),
-                      leading: CircleAvatar(
-                        radius: 30,
-                        backgroundImage: NetworkImage(
-                          info[index]['profilePic'].toString(),
-                        ),
-                      ),
-                      trailing: Text(
-                        info[index]['time'].toString(),
-                        style:
-                            const TextStyle(fontSize: 13, color: Colors.grey),
+      child: ListView.builder(
+        shrinkWrap: true,
+        itemCount: info.length,
+        itemBuilder: (context, index) {
+          return Column(
+            children: [
+              InkWell(
+                onTap: () {},
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: ListTile(
+                    title: Text(
+                      info[index]['name'].toString(),
+                      style: const TextStyle(fontSize: 18),
+                    ),
+                    subtitle: Padding(
+                      padding: const EdgeInsets.only(top: 6),
+                      child: Text(
+                        info[index]['message'].toString(),
+                        style: const TextStyle(fontSize: 15),
                       ),
                     ),
+                    leading: CircleAvatar(
+                      radius: 30,
+                      backgroundImage: NetworkImage(
+                        info[index]['profilePic'].toString(),
+                      ),
+                    ),
+                    trailing: Text(
+                      info[index]['time'].toString(),
+                      style: const TextStyle(fontSize: 13, color: Colors.grey),
+                    ),
                   ),
-                );
-              },
-            ),
-          ),
-          const Divider(
-            color: dividerColor,
-            indent: 85,
-          )
-        ],
+                ),
+              ),
+              const Divider(
+                color: dividerColor,
+                indent: 85,
+              )
+            ],
+          );
+        },
       ),
     );
   }
