a4c13466445189c9596afecbd03078940cb9b232
alexacolbe <ibegbulam.ua@gmail.com>
2023-07-12 23:40:53 +0100

    added device preview and updated packages

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:device_preview/device_preview.dart';
 import 'package:whatsappclone/colors.dart';
@@ -7,15 +6,9 @@ import 'package:whatsappclone/screens/mobile_screen_layout.dart';
 import 'package:whatsappclone/screens/web_screen_layout.dart';
 
 void main() {
-  runApp(
-    //DevicePreview(
-    
-    
-    //enabled: !kReleaseMode,
-    //builder: (BuildContext context) => 
-    const MyApp(),
-  //)
-  );
+  runApp(DevicePreview(
+    builder: (BuildContext context) => const MyApp(),
+  ));
 }
 
 class MyApp extends StatelessWidget {
@@ -25,9 +18,6 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-     // useInheritedMediaQuery: true,
-     // locale: DevicePreview.locale(context),
-      //builder: DevicePreview.appBuilder,
       theme: ThemeData.dark().copyWith(
         scaffoldBackgroundColor: backgroundColor,
       ),
